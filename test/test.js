718cdff37634c56ad63a3069f66696cd2933f5f4
khyder <92006563+khyder@users.noreply.github.com>
2022-01-04 17:20:35 +0530

    updated

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('node-app', () => {
     describe('GET /', function() {
         it('respond with hello konvergen', (done) => {
             //navigate to root and check the response is "hello world"
-            request(app).get('/').expect('hello konvergen').end(done);
+            request(app).get('/').expect('hello World..!!').end(done);
         });
     });
 })
